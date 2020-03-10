ca87761f0180a40db1691e937e10bab0a05dc02c
Bekir Ozturk <bozturk@microsoft.com>
2020-03-10 13:29:54 +0100

    Log output is flushed

--- src/InsertionsClient/Common/Logging/InsertionsConsoleTraceListener.cs
+++ src/InsertionsClient/Common/Logging/InsertionsConsoleTraceListener.cs
@@ -1,23 +1,15 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Diagnostics;
-using System.Text;
 
 namespace Microsoft.Net.Insertions.Common.Logging
 {
     public sealed class InsertionsConsoleTraceListener : ConsoleTraceListener
     {
-        private StringBuilder _stringBuilder = new StringBuilder(512);
-
         public override void WriteLine(string message)
         {
-            _stringBuilder.Clear();
-            _stringBuilder.Append(DateTime.Now.ToString("dd-M-yyyy hh:mm:ss.ffffff"));
-            _stringBuilder.Append("|thread:").Append(Environment.CurrentManagedThreadId);
-            _stringBuilder.Append("|").Append(message);
-
-            base.WriteLine(_stringBuilder.ToString());
+            base.WriteLine(Utilities.MessageToLogString(message));
+            Flush();
         }
     }
 }
--- src/InsertionsClient/Common/Logging/InsertionsTextWriterTraceListener.cs
+++ src/InsertionsClient/Common/Logging/InsertionsTextWriterTraceListener.cs
@@ -1,15 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Diagnostics;
-using System.Text;
 
 namespace Microsoft.Net.Insertions.Common.Logging
 {
     internal sealed class InsertionsTextWriterTraceListener : TextWriterTraceListener
     {
-        private StringBuilder _stringBuilder = new StringBuilder(512);
-
         internal InsertionsTextWriterTraceListener(string fileName, string listenerName)
             : base(fileName, listenerName) 
         {
@@ -18,12 +14,8 @@ namespace Microsoft.Net.Insertions.Common.Logging
 
         public override void WriteLine(string message)
         {
-            _stringBuilder.Clear();
-            _stringBuilder.Append(DateTime.Now.ToString("dd-M-yyyy hh:mm:ss.ffffff"));
-            _stringBuilder.Append("|thread:").Append(Environment.CurrentManagedThreadId);
-            _stringBuilder.Append("|").Append(message);
-
-            base.WriteLine(_stringBuilder.ToString());
+            base.WriteLine(Utilities.MessageToLogString(message));
+            Flush();
         }
     }
 }
