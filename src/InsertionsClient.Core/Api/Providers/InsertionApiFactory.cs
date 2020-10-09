b57eef376e4e2d4a19542bc16087a3ae9fcc5e3a
William Li <wul@microsoft.com>
2020-10-09 08:58:56 -0700

    Code review

--- src/InsertionsClient.Core/Api/Providers/InsertionApiFactory.cs
+++ src/InsertionsClient.Core/Api/Providers/InsertionApiFactory.cs
@@ -4,6 +4,11 @@ using System;
 
 namespace Microsoft.DotNet.InsertionsClient.Api.Providers
 {
+    // IInsertionApiFactory is not use anywhere. Also, an interface to a factory is many layer of dependency injection, is
+    // that necessary?
+    
+    // InsertionApiFactory is not used in dependency injection. It should just be the constructor of InsertionApi.
+    // There is no use for IInsertionApi too.
     public sealed class InsertionApiFactory : IInsertionApiFactory
     {
         public IInsertionApi Create(TimeSpan? maxWaitSeconds = null, TimeSpan? maxDownloadSeconds = null, int ? maxConcurrency = null)
